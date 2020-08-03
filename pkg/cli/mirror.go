5e0f552435f4d1d6b7b1b7ea40a63e3d8f40c07a
Pierce Bartine <pbartine@salesforce.com>
2020-08-02 22:52:14 -0700

    Add info to mirror command help

--- pkg/cli/mirror.go
+++ pkg/cli/mirror.go
@@ -10,16 +10,22 @@ import (
 	"github.com/pbar1/swim/pkg/mirror"
 )
 
-const (
-	pool    = 10
-	timeout = 6 * time.Second
+var (
+	pool    int
+	timeout time.Duration
 )
 
 // getCmd represents the create command
 var mirrorCmd = &cobra.Command{
-	Use:   "mirror",
+	Use:   "mirror [DIVISION]",
 	Short: "Download all times available for a given parameter space",
-	Long:  `Download all times available for a given parameter space`,
+	Long: `Enumerates all possible NCAA Event Rank Search parameters for SCY. This
+includes both male and female, all named date ranges, all conferences, and all legal
+competition events. Results are saved to disk in CSV files corresponding to the
+search parameters that were used for that file. No results file will be written if
+the query returns an error.
+
+Values for DIVISION are one of: d1|d2|d3`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			log.Fatalln("must give argument: d1|d2|d3")
@@ -39,4 +45,6 @@ var mirrorCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(mirrorCmd)
+	mirrorCmd.Flags().IntVarP(&pool, "pool-size", "p", 10, "Number of concurrent threads")
+	mirrorCmd.Flags().DurationVarP(&timeout, "search-timeout", "t", 6*time.Second, "Seconds to allow searches before timing out")
 }
